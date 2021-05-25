635e77753765eab395a9a570b6f2cf04f2989741
mrWD <lvigtor@gmail.com>
2021-05-25 12:00:11 +0600

    Add AdMob.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Platform } from 'react-native';
+import { AdMobBanner } from 'expo-ads-admob';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
@@ -9,9 +10,13 @@ import Navigation from './navigation';
 
 import { Store } from './store';
 
+const ANDROID_UNIT_ID = 'ca-app-pub-9068470363255705/6926520094';
+const IOS_UNIT_ID = 'ca-app-pub-9068470363255705/7730667000';
+
 const App = () => {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const adUnitID = Platform.OS === 'ios' ? IOS_UNIT_ID : ANDROID_UNIT_ID;
 
   if (!isLoadingComplete) {
     return null;
@@ -21,7 +26,13 @@ const App = () => {
     <Store>
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+
+        <AdMobBanner
+          bannerSize="banner"
+          adUnitID={adUnitID}
+          servePersonalizedAds
+          onDidFailToReceiveAdWithError={console.error}
+        />
       </SafeAreaProvider>
     </Store>
   );
